d9d67adebb179b933a9ef037ef620e2fdd0b3a32
Maxim <maxim_sdrv@mail.ru>
2021-02-07 22:02:40 +0300

    correct implementation std::vector and fix problem using resource file, it was solved by added script in CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-vector<int> DataFile(vector<int> &connect){
+vector<int> DataFile(vector<int> &some_vector){
     int line;
-   ifstream in("D:\\ms.sidorov.project\\AlgorithmsRobertSadgewick\\Pairs");
+   ifstream in("Pairs");
    if (in.is_open()){
        while (in >> line){
-           connect.push_back(line);
+           some_vector.push_back(line);
        }
    }
    else {
@@ -20,34 +20,29 @@ vector<int> DataFile(vector<int> &connect){
 
    cout << "End of program" << endl;
 
-   return connect;
+   return some_vector;
 }
 
 void Find(){
     static const int N = 10000;
-    const int size = 24;
-    int p, q, id[N];
-    vector<int> connect;
-    DataFile(connect);
-    for (const int& i : connect){
-        cout << i << " " << endl;
-    }
-    int connect_point[size] = {3, 4, 4, 9, 8, 0, 2, 3, 5, 6, 2, 9, 5, 9,
-                               7, 3, 4, 8, 5, 6, 0, 2, 6, 1};
-    for (int i = 0; i < N; i++){
-        id[i] = i;
+    int p, q;
+    vector<int> some_vector;
+    vector<int> connect_point;
+    DataFile(some_vector);
+    for (int i = 0; i < N; ++i){
+        connect_point.push_back(i);
     }
 
-    for (int i = 0; i < size; ++i) {
-        p = connect_point[i];
-        q = connect_point[i + 1];
-            int t = id[p];
-            if (t == id[q]) {
+    for (int i = 0; i < some_vector.size(); ++i) {
+        p = some_vector[i];
+        q = some_vector[i + 1];
+            int t = connect_point.at(p);
+            if (t == connect_point.at(q)){
                 continue;
             }
             for (int i = 0; i < N; ++i) {
-                if (id[i] == t) {
-                    id[i] = id[q];
+                if (connect_point.at(i) == t) {
+                    connect_point.at(i) = connect_point.at(q);
                 }
             }
             cout << " " << p << " " << q << " connect established" << endl;
